bdfcca34ac7135aa5604680247f481b144458481
David Smith <david.smith@mmbnetworks.com>
2019-04-12 09:28:21 -0400

    Updated with the License text.

--- update_xml.sh
+++ update_xml.sh
@@ -19,3 +19,15 @@ sed -i '' 's/ xmlns:zcl="http:\/\/zigbee.org\/zcl\/clusters">/>/g' *.xml
 sed -i '' 's/<classification role="\([^"]*\)" picsCode="\([^"]*\)" \/>/<classification hierarchy="base" role="\1" picsCode="\2" \/>/g' *.xml
 sed -i '' 's/<classification hierarchy="\([^"]*\)" role="application" picsCode="\([^"]*\)" \/>/<classification hierarchy="\1" role="application" picsCode="\2" primaryTransaction="" \/>/g' *.xml
 sed -i '' 's/<classification hierarchy="base"/<classification/g' *.xml
+
+for filename in *.xml; do
+	sed -i '' '$!N;s/<?xml version="1.0"?>\n<zcl/<?xml version="1.0"?>\
+<!-- \
+Zigbee Alliance owns the copyright to the text and content displayed or \
+included in this document (including in PDF, XML files and other formats) in \
+all forms of media, which copyright is protected by the copyright laws of the \
+United States and by international treaties.  Full text of licensing terms \
+applicable to this document can be found in the LICENSE.md file.\
+-->\
+<zcl/g' "$filename"
+done
